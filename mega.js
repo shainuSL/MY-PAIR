dca958544a375f1fa5ad1ee079b5e90763168110
shainuSL <shamindaravishka21@gmail.com>
2025-01-13 15:32:22 +0530

    Update mega.js

--- mega.js
+++ mega.js
@@ -1,7 +1,7 @@
 const mega = require("megajs");
 const auth = {
-  email: "Enter your mega email",
-  password: "Enter your mega password",
+  email: "shamindaravishka21@gmail.com",
+  password: "RRRR1234ss@2005",
   userAgent:
     "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Safari/537.36 Edge/12.246",
 };
