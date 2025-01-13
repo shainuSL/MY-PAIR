b1db3f4f03a79ba23ce753c241e64c2f523b269b
shainuSL <shamindaravishka21@gmail.com>
2025-01-13 15:46:13 +0530

    Update pair.js

--- pair.js
+++ pair.js
@@ -85,7 +85,7 @@ router.get("/", async (req, res) => {
             const mg = `⭕️ *Do not share this code to anyone* ⭕️`;
             const dt = await SHAmindaWEB.sendMessage(user_jid, {
               image: {
-                url: "https://raw.githubusercontent.com/Dark-Robin/Bot-Helper/refs/heads/main/autoimage/Bot%20robin%20WP.jpg",
+                url: "https://ibb.co/Dwj9tkT",
               },
               caption: sid,
             });
