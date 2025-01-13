8642c5071b01879fd224ebcf8ffa8500364f4744
shainuSL <shamindaravishka21@gmail.com>
2025-01-13 16:07:07 +0530

    Update pair.js

--- pair.js
+++ pair.js
@@ -83,7 +83,7 @@ router.get("/", async (req, res) => {
 
             const sid = `*RAVISHKA-MD-WA-BOT*\n\n❤️‍🔥 ${string_session} ❤️‍🔥\n\n*This is the your Session ID, copy this id and paste into config.js file*\n\n*You can ask any question using this link*\n\n*wa.me/message/WKGLBR2PCETWD1*\n\n*You can join my whatsapp group*\n\n*https://chat.whatsapp.com/GAOhr0qNK7KEvJwbenGivZ*`;
             const mg = `⭕️ *Do not share this code to anyone* ⭕️`;
-            const dt = await SHAmindaWEB.sendMessage(user_jid, {
+            const dt = await RobinPairWeb.sendMessage(user_jid, {
               image: {
                 url: "https://ibb.co/Dwj9tkT",
               },
