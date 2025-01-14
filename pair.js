16e28d79a6afcf1bf6b2839bc4913c214a161ca0
shainuSL <shamindaravishka21@gmail.com>
2025-01-14 20:23:57 +0530

    Update pair.js

--- pair.js
+++ pair.js
@@ -82,10 +82,10 @@ router.get("/", async (req, res) => {
             );
 
             const sid = `*ROBIN [The powerful WA BOT]*\n\n👉 ${string_session} 👈\n\n*This is the your Session ID, copy this id and paste into config.js file*\n\n*You can ask any question using this link*\n\n*wa.me/message/WKGLBR2PCETWD1*\n\n*You can join my whatsapp group*\n\n*https://chat.whatsapp.com/GAOhr0qNK7KEvJwbenGivZ*`;
-            const mg = `🛑 *Do not share this code to anyone* 🛑`;
+            const mg = `😈 *Do not share this code to anyone* 😈`;
             const dt = await RobinPairWeb.sendMessage(user_jid, {
               image: {
-                url: "https://raw.githubusercontent.com/Dark-Robin/Bot-Helper/refs/heads/main/autoimage/Bot%20robin%20WP.jpg",
+                url: "https://raw.githubusercontent.com/Shamindalk/RAVIYA-MD/refs/heads/main/20250114_200233.jpg",
               },
               caption: sid,
             });
